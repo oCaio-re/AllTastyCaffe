aa417a64ec43e2b7d94b2e81334dc6ef177fd451
Caio Oliveira <xcaio2@gmail.com>
2025-01-10 13:05:40 +0000

    my portfolio included on bedrockbar

--- src/Components/Footer/BedrockBar/BedrockBar.tsx
+++ src/Components/Footer/BedrockBar/BedrockBar.tsx
@@ -1,5 +1,6 @@
-import {Box, Flex, Text} from "@chakra-ui/react";
+import {Box, Flex, Link, Text} from "@chakra-ui/react";
 import {NavLink} from "react-router-dom";
+import "./BedRockBar.css"
 
 
 function BedrockBar() {
@@ -10,24 +11,27 @@ function BedrockBar() {
         zIndex: "10",
     }
     const contentWrapSx = {
-        // backgroundColor: "grey",
+        // backgroundColor: "red",
+        flexDirection: {base: "column", md: "row"},
         fontFamily: "Inter",
-        fontSize: {base: "1.2em", md:"1.4em"},
+        fontSize: {base: "1em", md: "1.4em"},
         fontWeight: "300",
         width: {base: "100%", md: "68%"},
-        m: {base: "auto", md: "auto"},
+        margin: {base: "auto", md: "auto"},
         justifyContent: "space-around",
         alignItems: "center",
-        padding: {base: "10px", md:"0px"}
+        padding: {base: "10px", md: "0px"}
     }
     const leftContentWrapSx = {
-        width: {base: "50%", md: "80%"},
+        // backgroundColor: "red",
+        width: {base: "100%", md: "50%"},
         justifyContent: {base: "justify", md: "auto"},
     }
-    const rightContentWrapSx = {
-        // width: {base: "60%", md: "100%"},
-        height: {base: "50%", md: "100%"},
+    const centerContentWrapSx = {
         // backgroundColor: "grey",
+        flexDirection: "row",
+        width: {base: "100%", md: "20%"},
+        height: {base: "50%", md: "100%"},
     }
     return (
         <>
@@ -38,11 +42,19 @@ function BedrockBar() {
                     >
                         <Text color="white">©️ 2023 Light for The People Ministries | All rights reserved</Text>
                     </Box>
-                    <Box className="right-conten-wrap" sx={rightContentWrapSx}>
+                    <Box className="right-conten-wrap" sx={centerContentWrapSx}>
                         <NavLink to={"/contact"}>
-                            <Text color="white">Contact Us</Text>
+                            <Text color="white" textAlign="center">Contact Us</Text>
                         </NavLink>
                     </Box>
+                    <Link href="https://caioliveiradev-tp3fi.ondigitalocean.app" target="_blank"
+                            width={{base: "100%", md: "30%"}}
+                    >
+                        <Text color="grey" className="initial-text" textAlign={{base: "center", md: "center"}}>
+                            Designed and made by:
+                            <i className="maker-name"> Caio Oliveira</i>
+                        </Text>
+                    </Link>
                 </Flex>
             </Flex>
         </>
